08e91d6e2e5c3693552ae45657caf92e0990dbe9
sethu1927 <sethu99vg@gmail.com>
2024-03-03 19:40:36 +0530

    demo app file

---
+++ lib/mainpage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class newmainpage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => newpage();
+}
+
+class newpage extends State<newmainpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+
+
+    );
+  }
+}
